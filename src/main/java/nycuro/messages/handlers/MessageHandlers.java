75a9a794dc4cd155461e42c456a86b0e821fe269
NycuRO <nghergu17@gmail.com>
2019-04-30 22:38:33 +0300

    finally fix

--- src/main/java/nycuro/messages/handlers/MessageHandlers.java
+++ src/main/java/nycuro/messages/handlers/MessageHandlers.java
@@ -8,6 +8,7 @@ import cn.nukkit.event.player.PlayerDeathEvent;
 import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.event.player.PlayerQuitEvent;
 import nycuro.API;
+import nycuro.Loader;
 
 /**
  * author: NycuRO
@@ -19,6 +20,7 @@ public class MessageHandlers implements Listener {
     @EventHandler(priority = EventPriority.NORMAL)
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+        Loader.isOnSpawn.put(player.getName(), true);
         event.setJoinMessage("");
         API.getMessageAPI().sendJoinMessages(player);
     }
@@ -26,6 +28,7 @@ public class MessageHandlers implements Listener {
     @EventHandler
     public void onLeave(PlayerQuitEvent event) {
         Player player = event.getPlayer();
+        Loader.isOnSpawn.removeBoolean(player.getName());
         event.setQuitMessage("");
     }
 
