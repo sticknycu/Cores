6c2e20bd3993c797aa0b843dd45d6ef74f79ae8f
Gabitzuu <contact@gabitzuu.me>
2019-07-17 21:49:44 +0300

    remove getters and setters as well

--- src/main/java/nycuro/utils/vote/objects/Mechanic.java
+++ src/main/java/nycuro/utils/vote/objects/Mechanic.java
@@ -6,20 +6,4 @@ import lombok.Data;
 public class Mechanic {
     public int dropParty;
     public long timeDropParty;
-
-    public int getDropParty() {
-        return dropParty;
-    }
-
-    public void setDropParty(int dropParty) {
-        this.dropParty = dropParty;
-    }
-
-    public long getTimeDropParty() {
-        return timeDropParty;
-    }
-
-    public void setTimeDropParty(long timeDropParty) {
-        this.timeDropParty = timeDropParty;
-    }
 }
