bf1efeea63f310c06ef9c07bfb24eb5b1271b8c9
NycuRO <nghergu17@gmail.com>
2019-07-27 20:29:14 +0300

    changes bossbar

--- src/main/java/nycuro/chat/api/MessageAPI.java
+++ src/main/java/nycuro/chat/api/MessageAPI.java
@@ -1171,27 +1171,27 @@ public class MessageAPI {
         }
     }
 
-    public String getMessageBossBar(Player player, int level, double necesarry, double count) {
+    public String getMessageBossBar(Player player, int level, double necesarry, double count, String title) {
         int lang = Database.profileProxy.getOrDefault(player.getName(), new ProfileProxy(player.getName(), 0,0,0,0)).getLanguage();
         switch (lang) {
             case 0:
-                STRING_BOSSBAR = "         mcpe.chzone.eu\n\n       §6§l»§r--- Level §6§l" + level + "§r ---§6§l« §r\n  §rNecesarry XP: §6" + Loader.round(count, 2) + "§7/§6" + necesarry;
+                STRING_BOSSBAR = "         " + title + "\n\n      §6§l»§r--- Level §6§l" + level + "§r ---§6§l« §r\n   §rNecesarry XP: §6" + Loader.round(count, 2) + "§7/§6" + necesarry;
                 break;
             case 1:
-                STRING_BOSSBAR = "     mcpe.chzone.eu\n\n   §6§l»§r--- Nivel §6§l" + level + "§r ---§6§l« §r\n §rXP Necesar: §6" + Loader.round(count, 2) + "§7/§6" + necesarry;
+                STRING_BOSSBAR = "     " + title + "\n\n   §6§l»§r--- Nivel §6§l" + level + "§r ---§6§l« §r\n §rXP Necesar: §6" + Loader.round(count, 2) + "§7/§6" + necesarry;
                 break;
         }
         return STRING_BOSSBAR;
     }
 
-    public String getMessageInArenaBossBar(Player player, double bosshp) {
+    public String getMessageInArenaBossBar(Player player, double bosshp, String title) {
         int lang = Database.profileProxy.getOrDefault(player.getName(), new ProfileProxy(player.getName(), 0,0,0,0)).getLanguage();
         switch (lang) {
             case 0:
-                STRING_BOSSBAR = "         mcpe.chzone.eu\n\n    §6§l»§r-- Boss §6§l§r --§6§l« §r\n      §rHP: §6" + bosshp + "§7/" + "§6100";
+                STRING_BOSSBAR = "         " + title + "\n\n    §6§l»§r-- Boss §6§l§r --§6§l« §r\n      §rHP: §6" + bosshp + "§7/" + "§6100";
                 break;
             case 1:
-                STRING_BOSSBAR = "         mcpe.chzone.eu\n\n   §6§l»§r-- Boss §6§l§r --§6§l« §r\n  §rViata: §6" + bosshp + "§7/" + "§6100";
+                STRING_BOSSBAR = "         " + title + "\n\n   §6§l»§r-- Boss §6§l§r --§6§l« §r\n  §rViata: §6" + bosshp + "§7/" + "§6100";
                 break;
         }
         return STRING_BOSSBAR;
--- src/main/java/nycuro/tasks/BossBarTask.java
+++ src/main/java/nycuro/tasks/BossBarTask.java
@@ -2,6 +2,7 @@ package nycuro.tasks;
 
 import cn.nukkit.Player;
 import cn.nukkit.scheduler.Task;
+import cn.nukkit.utils.TextFormat;
 import nycuro.api.API;
 import nycuro.Loader;
 import nycuro.database.Database;
@@ -14,8 +15,11 @@ import nycuro.database.objects.ProfileSkyblock;
  */
 public class BossBarTask extends Task {
 
+    private int k = -1;
+
     @Override
     public void onRun(int i) {
+        k++;
         for (Player player : API.getMainAPI().getServer().getOnlinePlayers().values()) {
             ProfileSkyblock profile = Database.profileSkyblock.get(player.getName());
 
@@ -30,13 +34,27 @@ public class BossBarTask extends Task {
                     count = profile.getExperience();
                     profile.setTime(profile.getTime() + 1000);
                 }
+                String title = "&m&c&p&e&.&c&h&z&o&n&e&.&e&u";
+                String[] split = title.split("&");
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < split.length; j++) {
+                    sb.append(split[j]);
+                    if (j == k) {
+                        sb.append("&c");
+                    } else {
+                        sb.append("&r");
+                    }
+                }
+                if (k == split.length) k = -1;
+                String p = TextFormat.colorize(sb.toString());
+
                 String message = "";
                 float health = 100F;
                 if (API.getMechanicAPI().isOnArena(player) && API.getMechanicAPI().getBossHealth() != 0) {
                     health = API.getMechanicAPI().getBossHealth();
-                    message = API.getMessageAPI().getMessageInArenaBossBar(player, Loader.round((double) API.getMechanicAPI().getBossHealth(), 2));
+                    message = API.getMessageAPI().getMessageInArenaBossBar(player, Loader.round((double) API.getMechanicAPI().getBossHealth(), 2), p);
                 } else {
-                    message = API.getMessageAPI().getMessageBossBar(player, level, necessary, count);
+                    message = API.getMessageAPI().getMessageBossBar(player, level, necessary, count, p);
                 }
                 API.getMainAPI().bossbar.get(player.getUniqueId()).setLength(health);
                 API.getMainAPI().bossbar.get(player.getUniqueId()).setText(message);
